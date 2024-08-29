94f5520deba03bca99410971bc7fd4ff29bcecda
MaximilianToe <mtoenies@outlook.de>
2024-08-29 20:51:43 +0200

    hypr update

--- uploadLinuxSettings.sh
+++ uploadLinuxSettings.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp -r ~/.config/hypr ~/.config/waybar ~/.config/clipse  ~/.dotfiles/.config
+cp -r ~/.wallpapers ~/.dotfiles/
 sudo git add .
 sudo git commit -m "hypr update"
 git push -u origin main
