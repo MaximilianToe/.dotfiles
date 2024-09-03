cd8d76b804b102ff840bd53c758937953b403b40
MaximilianToe <mtoenies@outlook.de>
2024-09-03 10:18:03 +0200

    update

--- uploadNvimTerminalSettings.sh
+++ uploadNvimTerminalSettings.sh
@@ -6,7 +6,7 @@ cp -rf /home/max/.zshrc /home/max/.dotfiles
 cp -rf /home/max/.oh-my-zsh/custom /home/max/.dotfiles/.oh-my-zsh
 cp -rf /home/max/.p10k.zsh /home/max/.dotfiles
 
-sudo git add .
-sudo git commit -m "update"
+git add .
+git commit -m "update"
 git push -u origin main
 
