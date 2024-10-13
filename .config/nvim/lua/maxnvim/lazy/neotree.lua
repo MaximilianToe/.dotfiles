3f8a18f90f9e2b764073665054dfcc68b09eef4c
MaximilianToe <mtoenies@outlook.de>
2024-10-13 09:02:13 +0200

    fixed neotree

--- .config/nvim/lua/maxnvim/lazy/neotree.lua
+++ .config/nvim/lua/maxnvim/lazy/neotree.lua
@@ -7,4 +7,19 @@ return {
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     branch = "v3.x",
+    config = function()
+        vim.keymap.set("n", "<leader>pv", "<cmd>:Neotree position=current  reveal<cr>")
+        require("neo-tree").setup({
+            hijack_netrw_behavior = "open_current",
+            mappings = {
+                ["z"] = {
+                    "toggle_node",
+                    nowait = true
+                }
+            },
+            window = {
+                position = "current",
+            },
+        })
+    end
 }
